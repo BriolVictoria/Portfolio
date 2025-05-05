7eaa35df5229fe41c80e39555f5f61b733bbce7b
BriolVictoria <victoria.briol@student.hepl.be>
2025-05-05 14:25:02 +0200

    Added footer css

--- wp-content/themes/portfolio/footer.php
+++ wp-content/themes/portfolio/footer.php
@@ -1,28 +1,32 @@
 </main>
 
 <footer>
+    <div class="ligne_vertical"></div>
+
+    <div class="footer_columns">
+
 
     <div class="coordonnees">
         <h4 class="titre_footer">Coordonnées</h4>
-        <p class="email_pro_footer">Victoria.briol@student.hepl.be</p>
-        <p class="email_footer">Victoriabriol5@gmail.com</p>
+        <p class="content">Victoria.briol@student.hepl.be</p>
+        <p class="content">Victoriabriol5@gmail.com</p>
     </div>
 
     <div class="sociaux">
         <h4 class="titre_footer">Réseaux sociaux</h4>
-        <a class="insta_footer" href="https://www.instagram.com/victoria_briol/">Instagram : victoria_briol</a>
-        <a class="github_footer" href="https://github.com/BriolVictoria">GitHub : VictoriaBriol</a>
+        <a class="content" href="https://www.instagram.com/victoria_briol/">Instagram : victoria_briol</a>
+        <a class="content" href="https://github.com/BriolVictoria">GitHub : VictoriaBriol</a>
     </div>
 
     <div class="navigation">
         <h4 class="titre_footer">Navigation</h4>
-        <a class="lien_footer" href="#accueil">Accueil</a>
-        <a class="lien_footer" href="#decouvrir">Me découvrir</a>
-        <a class="lien_footer" href="#projets">Mes projets</a>
-        <a class="lien_footer" href="#contact">Contact</a>
+        <a class="content" href="#accueil">Accueil</a>
+        <a class="content" href="#decouvrir">Me découvrir</a>
+        <a class="content" href="#projets">Mes projets</a>
+        <a class="content" href="#contact">Contact</a>
     </div>
-
-    <p>© <?= get_bloginfo('name'); ?></p>
+    </div>
+    <p class="blog_info">© Tous droit réservé à <?= get_bloginfo('name'); ?></p>
 
 </footer>
 
