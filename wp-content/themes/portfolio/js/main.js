8879fc1cd3840c7a94d962258372760721352299
BriolVictoria <victoria.briol@student.hepl.be>
2025-06-14 16:54:14 +0200

    Added burger menu

--- wp-content/themes/portfolio/js/main.js
+++ wp-content/themes/portfolio/js/main.js
@@ -1,3 +1,4 @@
+/*
 const elements = document.querySelectorAll('.projet, .titre_project, .description_project, .bouton_container_projet');
 
 function checkVisibility() {
@@ -13,3 +14,16 @@ function checkVisibility() {
 
 window.addEventListener('scroll', checkVisibility);
 checkVisibility(); // pour les éléments déjà visibles au chargement
+*/
+
+const burgerBtn = document.querySelector('.nav_bgm');
+const menu = document.querySelector('.droite');
+
+burgerBtn.addEventListener('click', () => {
+    menu.classList.toggle('active');
+
+    // Gérer l'attribut aria-expanded pour l'accessibilité
+    const expanded = burgerBtn.getAttribute('aria-expanded') === 'true' || false;
+    burgerBtn.setAttribute('aria-expanded', !expanded);
+});
+
